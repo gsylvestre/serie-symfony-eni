6379ddd600ee0fccfc3b0ac68b965992053576d6
gsylvestre <gsylvestre@gmail.com>
2020-12-15 16:17:08 +0100

    Affiche la liste et le détail des séries

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,30 +18,73 @@ class SerieController extends AbstractController
     /**
      * @Route("", name="list")
      */
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        //todo: aller chercher les séries en bdd
+        $series = $serieRepository->findBestSeries();
 
         return $this->render('serie/list.html.twig', [
-
+            "series" => $series
         ]);
     }
 
     /**
      * @Route("/details/{id}", name="details")
      */
-    public function details(int $id): Response
+    public function details(int $id, SerieRepository $serieRepository): Response
     {
-        //todo: aller chercher la série en bdd
+        $serie = $serieRepository->find($id);
 
-        return $this->render('serie/details.html.twig');
+        return $this->render('serie/details.html.twig', [
+            "serie" => $serie
+        ]);
     }
 
     /**
      * @Route("/create", name="create")
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        dump($request);
+        return $this->render('serie/create.html.twig');
+    }
+
+    /**
+     * @Route("/demo", name="em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //crée une instance de mon entité
+        $serie = new Serie();
+
+        //hydrate toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafsdf');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate(new \DateTime("- 6 month"));
+        $serie->setGenres('drama');
+        $serie->setOverview('bla bla bla');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('Canceled');
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        //$entityManager->remove($serie);
+
+        $serie->setGenres('comedy');
+
+        $entityManager->flush();
+
+        //$entityManager = $this->getDoctrine()->getManager();
+
         return $this->render('serie/create.html.twig');
     }
 }
